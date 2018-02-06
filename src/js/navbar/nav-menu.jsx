940091145231ec3f785ab128589c7f081bc51a3d
marcusbotelho <marcus.itec@gmail.com>
2018-02-06 16:58:43 -0300

    UPDATE: Add states on menu

--- src/js/navbar/nav-menu.jsx
+++ src/js/navbar/nav-menu.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 
 class NavMenu extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {menuAtivo: 'Home'};
+        this.alteraActive = this.alteraActive.bind(this);
+    }
+
+    alteraActive(titulo, self) {
+        self.setState({ menuAtivo: titulo })
+    }
+
     render(){
+        let self = this;
         let lista = this.props.menu.map(function (value) {
             return (
-                <li key={value.titulo}><a href={value.link}>{value.titulo}</a></li>
+                <li key={value.titulo} onClick={self.alteraActive.bind(null, value.titulo, self)} className={self.state.menuAtivo === value.titulo ? 'active' : ''}><a href={value.link}>{value.titulo}</a></li>
             );
         });
 
