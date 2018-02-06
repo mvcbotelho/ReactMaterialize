6622a09916ac2d3409d7de78f29383436fdb64d8
marcusbotelho <marcus.itec@gmail.com>
2018-02-06 15:56:54 -0300

    UPDATE: Modifications in card style

--- src/js/cartao/cartao.jsx
+++ src/js/cartao/cartao.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
 
 class Cartao extends React.Component {
-    render(){
+    render() {
         return (
             <div className="card sticky-action">
                 <div className="card-image waves-effect waves-block waves-light">
-                    <img className="activator" src="http://materializecss.com/images/office.jpg" />
+                    <img className="activator" src="http://materializecss.com/images/office.jpg"/>
                 </div>
                 <div className="card-content">
-                    <span className="card-title activator grey-text text-darken-4">Título do Cartão<i className="material-icons right">more_vert</i></span>
-                    <p><a href="#">link cartão</a></p>
+                    <span className="card-title activator grey-text text-darken-4">Título do Cartão
+                        <i className="material-icons right">more_vert</i></span>
                 </div>
                 <div className="card-action">
                     <p><a href="#">Ver mais</a></p>
                 </div>
                 <div className="card-reveal">
-                    <span className="card-title grey-text text-darken-4">Título<i className="material-icons right">close</i></span>
+                    <span className="card-title grey-text text-darken-4">Título<i
+                        className="material-icons right">close</i></span>
                     <p>Texto cartão!!!!</p>
                 </div>
             </div>
--- src/js/titulo/titulo.jsx
+++ src/js/titulo/titulo.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 class Titulo extends React.Component {
     render(){
-        return <h1>Cartões animados de React</h1>;
+        return <h1>Cartões animados com React</h1>;
     };
 }
 
