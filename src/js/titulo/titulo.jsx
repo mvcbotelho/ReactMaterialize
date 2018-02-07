a2c13890361ec7588cc324ceea17acd2bc77631c
marcusbotelho <marcus.itec@gmail.com>
2018-02-06 21:18:36 -0300

    UPDATE: Implement a clock

--- src/js/titulo/titulo.jsx
+++ src/js/titulo/titulo.jsx
@@ -1,8 +1,35 @@
 import React from 'react';
 
 class Titulo extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = { hora: new Date() };
+    }
+
+    componentDidMount(){
+        this.horaID = setInterval(
+            () => this.atualizaSeg(),
+            1000
+        );
+    }
+
+    atualizaSeg(){
+        this.setState({ hora: new Date() });
+    }
+
+    componentWillUnMount() {
+        clearInterval(this.horaID);
+    }
+
     render(){
-        return <h1>Cartões animados com React</h1>;
+        return (
+            <div>
+                <h1>Cartões animados com React</h1>
+                <p>{ this.state.hora.toLocaleString() }</p>
+            </div>
+
+        );
     };
 }
 
